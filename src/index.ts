f4fc37c1aed39a91f954d13af6b38b616dce4bc5
Pavol Varga <pavol.varga@gmail.com>
2023-10-31 11:50:12 +0100

    Add printing of help

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import { Command } from 'commander';
 import figlet from 'figlet';
 
+const program = new Command();
+
+program
+  .name('anki-analyzer')
+  .version('0.1.0', '-v, --version', 'Displays current version')
+  .description('App for analyzing exported Anki Decks');
+
+program
+  .option('-h, --help', 'Displays help');
+
+program.parse(process.argv);
+
 console.log(figlet.textSync('Anki Analyzer'));
+
+if (process.argv.length < 3) {
+  program.help();
+  process.exit();
+}
